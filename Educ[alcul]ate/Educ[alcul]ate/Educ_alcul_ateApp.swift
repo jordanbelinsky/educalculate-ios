a995af022baf12a5e85aaa27cb887c3ef93bd1cf
Jordan Belinsky <jordanbelinsky1@gmail.com>
2021-01-08 05:47:21 -0500

    Define base code for CardView
    completed 1.3

---
+++ Educ[alcul]ate/Educ[alcul]ate/Educ_alcul_ateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Educ_alcul_ateApp.swift
+//  Educ[alcul]ate
+//
+//  Created by Jordan Belinsky on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Educ_alcul_ateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
